d2b1e5973d3146219bd699159190480c683d847d
Tokur233 <tokur233@outlook.com>
2025-03-29 21:57:43 +0800

    更改目录结构并丰富gitignore

--- NumberTheory/modPow.h
+++ NumberTheory/modPow.h
@@ -1,7 +1,7 @@
 // 要求b为质数，且当a不是p的倍数下，可以用qpow(a,p-2)来求乘法逆元
 long long p = 998244353;
 
-// p为模数，
+// p为模数，该函数给出a的b次方 mod p的结果
 long long qpow(long long a, int b) {
     long long ans = 1;
     a = (a % p + p) % p;
