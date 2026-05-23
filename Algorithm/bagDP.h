b790d6dfbd35a6fdd8b11a892852e163b2309172
huangtz <1513069840@qq.com>
2026-05-23 13:47:11 +0800

    修改线段树, 修改背包dp

--- Algorithm/bagDP.h
+++ Algorithm/bagDP.h
@@ -14,7 +14,8 @@ pair<int, int> dp01bags(int bagSize, vector<int> &weight, vector<int> &value) {
 }
 
 // 多重背包
-int dpMultiBags(int n, int W, vector<int> &w, vector<int> &v, vector<int> &cnt) {
+int dpMultiBags(int n, int W, vector<int> &w, vector<int> &v,
+                vector<int> &cnt) {
     vector<int> dp(W + 1, 0);
     for (int i = 1; i <= n; i++) {
         for (int weight = W; weight >= w[i]; weight--) {
@@ -31,6 +32,7 @@ int dpMultiBags(int n, int W, vector<int> &w, vector<int> &v, vector<int> &cnt) 
 const int MAXN = 1e4 + 3;
 const int MAXW = 1e7 + 3;
 long long dp[MAXW];
+
 // 带 相同w使用最大v 优化的完全背包
 long long dpCompleteBags(int n, int capacity) {
     map<int, int> best_v;
@@ -50,7 +52,8 @@ long long dpCompleteBags(int n, int capacity) {
 }
 
 // 混合背包
-int dpMixedBags(int n, int W, vector<int> &w, vector<int> &v, vector<int> &cnt) {
+int dpMixedBags(int n, int W, vector<int> &w, vector<int> &v,
+                vector<int> &cnt) {
     vector<int> dp(W + 1, 0);
     for (int i = 1; i <= n; i++) {
         if (cnt[i] == 0) { // 如果数量没有限制使用完全背包的核心代码
@@ -60,10 +63,67 @@ int dpMixedBags(int n, int W, vector<int> &w, vector<int> &v, vector<int> &cnt) 
         } else { // 物品有限使用多重背包的核心代码，它也可以处理0-1背包问题
             for (int weight = W; weight >= w[i]; weight--) {
                 for (int k = 1; k * w[i] <= weight && k <= cnt[i]; k++) {
-                    dp[weight] = max(dp[weight], dp[weight - k * w[i]] + k * v[i]);
+                    dp[weight] =
+                        max(dp[weight], dp[weight - k * w[i]] + k * v[i]);
                 }
             }
         }
     }
     return dp[W];
+}
+
+// 1-base
+long long Dp01Bag(int n, long long c, vector<long long> &w,
+                  vector<long long> &v) {
+    vector<long long> dp(c + 1, 0);
+    for (int i = 1; i <= n; i++) {
+        for (long long j = c; j >= w[i]; j--) {
+            dp[j] = max(dp[j], dp[j - w[i]] + v[i]);
+        }
+    }
+    return dp[c];
+}
+
+long long DpMultiBag(int n, long long c, vector<long long> &w,
+                     vector<long long> &v, vector<long long> &k) {
+    vector<long long> nw(1); // 1-base
+    vector<long long> nv(1); // 1-base
+    for (int i = 1; i <= n; i++) {
+        long long c = 1;
+        while (k[i] > c) {
+            k[i] -= c;
+            nw.push_back(c * w[i]);
+            nv.push_back(c * v[i]);
+            c *= 2;
+        }
+        nw.push_back(k[i] * w[i]);
+        nv.push_back(k[i] * v[i]);
+    }
+    return Dp01Bag((int)nw.size(), c, nw, nv);
+}
+
+long long DpCompleteBag(int n, long long c, vector<long long> &w,
+                        vector<long long> &v) {
+    vector<long long> dp(c + 1, 0);
+    for (int i = 1; i <= n; i++) {
+        for (long long j = w[i]; j <= c; j++) {
+            dp[j] = max(dp[j], dp[j - w[i]] + v[i]);
+        }
+    }
+    return dp[c];
+}
+
+// group: 1-base, 1-base
+long long DpGroupBag(long long c, vector<long long> &w, vector<long long> &v,
+                     vector<vector<int>> &group) {
+    for (int k = 1; k <= group.size(); k++) {
+        for (long long i = c; i >= 0; i--) {
+            for (int j = 1; j <= group[k].size(); j++) {
+                if (i >= w[group[k][j]]) {
+                    dp[i] = max(dp[i], dp[i - w[group[k][j]]] + v[group[k][j]]);
+                }
+            }
+        }
+    }
+    return dp[c];
 }
--- DataStructure/segmentTree_2.h
+++ DataStructure/segmentTree_2.h
@@ -1,96 +1,161 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T>
-class SegTreeLazyRangeSet {
-    vector<T> tree, lazy;
-    vector<T> *arr;
-    vector<bool> ifLazy;
-    int n, root, n4, end;
+template <typename Node, typename Tag, typename RangeT = int>
+#if __cplusplus >= 202002LL
+    requires requires(Node n, const Node &l, const Node &r, const Tag &t,
+                      RangeT range) {
+        // Node需要支持构造基本结构, 用于构造tree_和ans的初始值
+        { Node{} } -> std::same_as<Node>;
+        // 需要支持构造不会对Node产生影响的Tag
+        { Tag{} } -> std::same_as<Tag>;
+        // 从左右子树更新父节点
+        { n.set(l, r) } -> std::same_as<void>;
+        // 从左右两侧的查询结果更新ans
+        { n.update(l) } -> std::same_as<void>;
+        // 懒标记更新闭区间对应的子树的根节点
+        { n.range_update(t, range, range) } -> std::same_as<void>;
+        // 支持从Node构造懒标记
+        { static_cast<Tag>(n) } -> std::same_as<Tag>;
+    }
+/*
+为防止Node{0}和Tag{0}存在特殊含义或者不能从单一元素构造(例如矩阵)
+使用Node{}和Tag{}作为默认值, 实际使用时需要根据具体情况重写对应的构造函数
+同时, 使得所有的出现的Node以及Tag均为默认构造得到或者由外部传入, 保证通用性
+*/
+#endif
+class SegTreeLazy {
+    vector<Node> tree_;
+    vector<Tag> lazy_;
+    vector<Node> *arr_;
+    vector<bool> if_lazy_;
+    RangeT n_;
+    RangeT root_;
+    RangeT n4_;
+    RangeT end_;
 
-    void maintain(int cl, int cr, int p) {
+    void maintain_(RangeT cl, RangeT cr, RangeT p) {
         int cm = cl + (cr - cl) / 2;
-        if (cl != cr && ifLazy[p]) {
+        if (cl != cr && if_lazy_[p]) {
+            lazy_[p * 2] = lazy_[p];
+            if_lazy_[p * 2] = 1;
 
-            lazy[p * 2] = lazy[p], ifLazy[p * 2] = 1; // NOLINT
-            lazy[p * 2 + 1] = lazy[p], ifLazy[p * 2 + 1] = 1;
+            lazy_[p * 2 + 1] = lazy_[p];
+            if_lazy_[p * 2 + 1] = 1;
 
-            tree[p * 2] = lazy[p] * (cm - cl + 1);
-            tree[p * 2 + 1] = lazy[p] * (cr - cm);
+            tree_[p * 2].range_update(lazy_[p], cl, cm);
+            tree_[p * 2 + 1].range_update(lazy_[p], cm + 1, cr);
 
-            lazy[p] = 0;
-            ifLazy[p] = 0;
+            lazy_[p] = Tag{};
+            if_lazy_[p] = 0;
         }
     }
 
-    T range_sum(int l, int r, int cl, int cr, int p) {
-        if (l <= cl && cr <= r)
-            return tree[p];
-
-        int m = cl + (cr - cl) / 2;
-        T sum = 0;
-
-        maintain(cl, cr, p);
-
-        if (l <= m)
-            sum += range_sum(l, r, cl, m, p * 2);
-        if (r > m)
-            sum += range_sum(l, r, m + 1, cr, p * 2 + 1);
-        return sum;
-    }
-
-    void range_set(int l, int r, T val, int cl, int cr, int p) {
+    Node range_query_(RangeT l, RangeT r, RangeT cl, RangeT cr, RangeT p) {
         if (l <= cl && cr <= r) {
-            lazy[p] = val;
-            ifLazy[p] = 1;
-            tree[p] = (cr - cl + 1) * val;
-            return;
+            return tree_[p];
         }
-        int m = cl + (cr - cl) / 2;
-        maintain(cl, cr, p);
-        if (l <= m)
-            range_set(l, r, val, cl, m, p * 2);
-        if (r > m)
-            range_set(l, r, val, m + 1, cr, p * 2 + 1);
-        tree[p] = tree[p * 2] + tree[p * 2 + 1];
+
+        RangeT m = cl + (cr - cl) / 2;
+        Node ans = Node{};
+
+        maintain_(cl, cr, p);
+
+        if (l <= m) {
+            ans.update(range_query_(l, r, cl, m, p * 2));
+        }
+        if (r > m) {
+            ans.update(range_query_(l, r, m + 1, cr, p * 2 + 1));
+        }
+        return ans;
     }
 
-    void build(int s, int t, int p) {
-        if (s == t) {
-            tree[p] = (*arr)[s];
+    void range_update_(RangeT l, RangeT r, Node val, RangeT cl, RangeT cr,
+                       RangeT p) {
+        if (l <= cl && cr <= r) {
+            lazy_[p] = static_cast<Tag>(val);
+            if_lazy_[p] = 1;
+            tree_[p].range_update(lazy_[p], cl, cr);
             return;
         }
-        int m = s + (t - s) / 2;
-        build(s, m, p * 2);
-        build(m + 1, t, p * 2 + 1);
-        tree[p] = tree[p * 2] + tree[p * 2 + 1];
+
+        RangeT m = cl + (cr - cl) / 2;
+        maintain_(cl, cr, p);
+
+        if (l <= m) {
+            range_update_(l, r, val, cl, m, p * 2);
+        }
+
+        if (r > m) {
+            range_update_(l, r, val, m + 1, cr, p * 2 + 1);
+        }
+
+        tree_[p].set(tree_[p * 2], tree_[p * 2 + 1]);
+    }
+
+    void build_(RangeT s, RangeT t, RangeT p) {
+        if (s == t) {
+            tree_[p] = (*arr_)[s];
+            return;
+        }
+
+        RangeT m = s + (t - s) / 2;
+
+        build_(s, m, p * 2);
+        build_(m + 1, t, p * 2 + 1);
+
+        tree_[p].set(tree_[p * 2], tree_[p * 2 + 1]);
     }
 
   public:
-    explicit SegTreeLazyRangeSet<T>(vector<T> v) {
-        n = v.size();
-        n4 = n * 4;
-        tree = vector<T>(n4, 0);
-        lazy = vector<T>(n4, 0);
-        ifLazy = vector<bool>(n4, 0);
-        arr = &v;
-        end = n - 1;
-        root = 1;
-        build(0, end, 1);
-        arr = nullptr;
+    explicit SegTreeLazy(std::vector<Node> v) {
+        n_ = v.size();
+        n4_ = n_ * 4;
+        tree_ = vector<Node>(n4_, Node{});
+        lazy_ = vector<Tag>(n4_, Tag{});
+        if_lazy_ = vector<bool>(n4_, 0);
+        arr_ = &v;
+        end_ = n_ - 1;
+        root_ = 1;
+        build_(0, end_, 1);
+        arr_ = nullptr;
     }
 
-    void show(int p, int depth = 0) {
-        if (p > n4 || tree[p] == 0)
-            return;
-        show(p * 2, depth + 1);
-        for (int i = 0; i < depth; ++i)
-            putchar('\t');
-        printf("%d:%d\n", tree[p], lazy[p]);
-        show(p * 2 + 1, depth + 1);
+    Node range_query(RangeT l, RangeT r) {
+        return range_query_(l, r, 0, end_, root_);
     }
 
-    T range_sum(int l, int r) { return range_sum(l, r, 0, end, root); }
+    void range_update(RangeT l, RangeT r, Node val) {
+        range_update_(l, r, val, 0, end_, root_);
+    }
+};
 
-    void range_set(int l, int r, T val) { range_set(l, r, val, 0, end, root); }
+template <typename T>
+struct Tag {
+    T tag;
+
+    explicit Tag() : tag(0) {}
+
+    explicit Tag(T tag) : tag(tag) {}
+};
+
+template <typename T, typename RangeT = int>
+class Node {
+
+    T val_;
+
+  public:
+    explicit Node(T val) : val_(val) {}
+
+    explicit Node() : val_(0) {}
+
+    void set(const Node &l, const Node &r) { val_ = max(l.val_, r.val_); }
+
+    void update(const Node kN) { val_ = max(val_, kN.val_); }
+
+    void range_update(const Tag<T> &tag, RangeT l, RangeT r) { val_ = tag.tag; }
+
+    T &get() { return val_; }
+
+    explicit operator Tag<T>() { return Tag<T>{val_}; }
 };
